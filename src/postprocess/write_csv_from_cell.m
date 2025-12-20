37c9237faef4f66cae45d3a80cc462d35bafa2e2
Makoto Yamakawa <mako.yamakawa@gmail.com>
2025-12-20 13:58:57 +0900

    feat: CSV出力時のRFC 4180準拠エスケープ処理を追加
    - カンマ、ダブルクォート、改行を含む値をダブルクォートで囲む
    - 値内のダブルクォートは""にエスケープ
    - 関数ドキュメントを追加

--- src/postprocess/write_csv_from_cell.m
+++ src/postprocess/write_csv_from_cell.m
@@ -1,4 +1,11 @@
 function write_csv_from_cell(fid, head, body, modeSS7)
+%write_csv_from_cell - セル配列をCSV形式でファイルに書き出す
+%
+% 入力:
+%   fid     - ファイル識別子 (fopenで取得)
+%   head    - ヘッダー部のセル配列
+%   body    - データ部のセル配列
+%   modeSS7 - SS7形式フラグ (省略時true、trueの場合<data>タグを挿入)
 
 if nargin==3
   modeSS7 = true;
@@ -9,10 +16,20 @@ if modeSS7
   fprintf(fid, '<data>\n');
 end
 write_csv_from_cell_(fid, body)
+
+return
 end
 
 function write_csv_from_cell_(fid, tab)
+%write_csv_from_cell_ - セル配列を1行ずつCSV出力する内部関数
+%
+% RFC 4180準拠のエスケープ処理を行う:
+%   - カンマ、ダブルクォート、改行を含む値はダブルクォートで囲む
+%   - 値内のダブルクォートは "" にエスケープ
+
 [n,m] = size(tab);
+
+% 空行の判定 (全セルが空の行はスキップ対象)
 isempty_row = true(1,n);
 for i=1:n
   for j=1:m
@@ -22,11 +39,13 @@ for i=1:n
   end
 end
 
+% 各行を出力
 for i=1:n
   if isempty_row(i)
     continue
   end
   for j=1:m
+    % 最終列以外はカンマ区切り
     if j==m
       delimeter = '';
     else
@@ -35,10 +54,17 @@ for i=1:n
     if isnumeric(tab{i,j})
       fprintf(fid, ['%g' delimeter], tab{i,j});
     else
-      fprintf(fid, ['%s' delimeter], tab{i,j});
+      val = tab{i,j};
+      % カンマ、ダブルクォート、改行を含む場合はクォートで囲む (RFC 4180)
+      if contains(val, {',', '"', newline, char(13)})
+        val = ['"' strrep(val, '"', '""') '"'];
+      end
+      fprintf(fid, ['%s' delimeter], val);
     end
   end
   fprintf(fid, '\n');
 end
+
+return
 end
 
