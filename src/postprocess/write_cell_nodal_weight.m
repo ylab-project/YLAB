eadbf19484ed6544ff91b5015a9e88f998604014
Makoto Yamakawa <makoy@xps8950.amal.tus.ac.jp>
2026-03-27 11:32:00 +0900

    feat: 節点重量表のSS7互換2行/節点フォーマット対応
    - write_cell_nodal_weight: 1行目に節点名+床自重、2行目に各自重+合計
    - 概算軸力TL列と<RE>タグ列を追加
    - fnode/faddnodeを使用した合計値の計算を修正

--- src/postprocess/write_cell_nodal_weight.m
+++ src/postprocess/write_cell_nodal_weight.m
@@ -1,5 +1,5 @@
 function [nwhead, nwbody] = write_cell_nodal_weight(com, result)
-%write_cell_nodal_weight - 節点重量表のセル配列を生成
+%write_cell_nodal_weight - 節点重量表のセル配列を生成（2行/節点）
 
 % 定数
 nn = com.nnode;
@@ -9,6 +9,8 @@ nstory = com.nstory;
 
 % 共通配列
 feqvec = result.felement(:,1);
+fnode = com.fnode(:,1);
+faddnode = com.faddnode(:,1);
 node = com.node;
 n2df = com.node.dof;
 sw = result.sw;
@@ -21,11 +23,13 @@ sw.fw = fw_;
 sw.fc = fc_;
 sw.f = f_;
 
-% --- 節点重量表 ---
+% --- 節点重量表（SS7互換: 2行/節点） ---
 nwhead = {'X軸', 'Y軸', '層', '床自重', '梁自重', '壁自重', '特殊荷重', ...
-  '柱自重', '補正', 'ﾌﾚｰﾑ外', '基礎重量', '合計', '概算軸力';
-  '', '', '', 'kN', 'kN', 'kN', 'kN', 'kN', 'kN', 'kN', 'kN', 'kN', 'kN'};
-nwbody = cell(nn, 13);
+  '柱自重', '補正', 'ﾌﾚｰﾑ外', '基礎重量', '合計', '概算軸力', ...
+  '概算軸力TL'; '', '', '', 'kN', 'kN', 'kN', 'kN', 'kN', ...
+  'kN', 'kN', 'kN', 'kN', 'kN', 'kN'};
+ncol = 15; % 14データ列 + <RE>列
+nwbody = cell(nn*2, ncol);
 innn = 1:nn;
 irow = 0;
 for iy = 1:nbly
@@ -42,17 +46,24 @@ for iy = 1:nbly
       if node.idrep(in) > 0
         continue
       end
-      irow = irow+1;
       idf = n2df(in,3);
+      floor_ = feqvec(idf) * 1.d-3;
+      nf_ = (fnode(idf)+faddnode(idf)) * 1.d-3;
+      total_ = (feqvec(idf)+sw.f(idf)) * 1.d-3 - nf_;
+      % 1行目: 節点名 + 床自重 + 小計
+      irow = irow + 1;
       nwbody{irow,1} = node.xname{in};
       nwbody{irow,2} = node.yname{in};
       nwbody{irow,3} = node.zname{in};
-      nwbody{irow,4} = fmt(feqvec(idf)*1.d-3);
+      nwbody{irow,4} = fmt(floor_);
+      nwbody{irow,12} = fmt(floor_);
+      % 2行目: 各自重 + 合計 + <RE>
+      irow = irow + 1;
       nwbody{irow,5} = fmt(sw.fg(idf)*1.d-3);
       nwbody{irow,6} = fmt(sw.fw(idf)*1.d-3);
       nwbody{irow,8} = fmt(sw.fc(idf)*1.d-3);
-      total_ = (feqvec(idf)+sw.f(idf))*1.d-3;
       nwbody{irow,12} = fmt(total_);
+      nwbody{irow,ncol} = '<RE>';
     end
   end
 end
